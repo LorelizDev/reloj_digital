56c077f0dd5f9e1d15ccc75223096fca8cc5fc4d
LorelizDev <loreliz.dev@gmail.com>
2023-07-06 20:16:58 +0200

    Implementacion del temporizador

--- reloj.py
+++ reloj.py
@@ -5,7 +5,7 @@ import threading, time
 from googletrans import Translator
 from pygame import mixer
 
-
+# ---------------------- FUNCIONALIDAD DEL RELOJ ----------------------
 def update_clock():
     label_hm.config(text=strftime ("%I:%M")) # Formato 12 horas
     label_s.config(text=strftime ("%S"))
@@ -14,6 +14,7 @@ def update_clock():
     label_date.config(text=f"{translated_day}, {strftime('%d/%m/%Y')}")
     label_s.after(900, update_clock)
 
+# ---------------------- FUNCIONALIDAD DE LA ALARMA ----------------------
 def switch():
     global is_on
 
@@ -44,6 +45,7 @@ def play_alarm():
     mixer.music.stop()
     mixer.quit()
 
+# ---------------------- FUNCIONALIDAD DEL CRONOMETRO ----------------------
 def start_stop_t():
     threading.Thread(target=start_stop).start()
 
@@ -84,7 +86,7 @@ def start_stop():
 def lap():
     global laps
     laps += 1
-    
+
     if laps == 1:
         lap_1.config(text=f'{chrono_h}:{chrono_m}:{chrono_s}')
     elif laps == 2:
@@ -115,33 +117,101 @@ def restore_chrono():
     else:
         start_button.config(text="Iniciar")
 
+# ---------------------- FUNCIONALIDAD DEL TEMPORIZADOR ----------------------
+def click_play():
+    global is_running
+
+    if timer_s.get() != "00" or timer_m.get() != "00" or timer_h.get() != "00":
+        if is_running:
+            timer_play.config(image = play, text="continue")
+            is_running = False
+        else:
+            timer_play.config(image = pause)
+            is_running = True
+        play_stop_t()
+
+def play_stop_t():
+    threading.Thread(target=play_stop).start()
+
+def play_stop():
+    global timer_hh, timer_mm, timer_ss, is_running
+
+    if timer_play.cget("text") == "play":
+        timer_hh = timer_h.get()
+        timer_mm = timer_m.get()
+        timer_ss = timer_s.get()
+    
+    if is_running:
+        while is_running:
+            label_timer.config(text=f'{timer_hh}:{timer_mm}:{timer_ss}')
+            if label_timer.cget("text") == "00:00:00":
+                threading.Thread(target=timer_alarm).start()
+                messagebox.showinfo(message="Se ha agotado el tiempo", title="Temporizador")
+                click_play()
+            elif int(timer_ss) == 0:
+                timer_ss = 59
+                if int(timer_mm) == 0:
+                    timer_mm = 59
+                    if int(timer_hh) != 0:
+                        timer_hh = str(int(timer_hh) - 1).zfill(2)
+                else:
+                    timer_mm = str(int(timer_mm) - 1).zfill(2)
+            else:
+                timer_ss = str(int(timer_ss) - 1).zfill(2)
+            time.sleep(1)
+
+def timer_alarm():
+    mixer.init()
+    mixer.music.load("audios/alarm-clock_2.mp3")
+    mixer.music.play()
+    time.sleep(4)
+    mixer.music.stop()
+    mixer.quit()
+
+def restore_timer():
+    global is_running    
+    label_timer.config(text="00:00:00")
+    timer_play.config(text="play", image=play)
+    is_running = False
+    play_stop_t()
 
 root = Tk()
 root.title("Reloj digital by LorelizDev")
+
 frame_hour = Frame(root)
 frame_hour.pack()
+
 tabs = Notebook(root)
 tabs.pack(pady=30, padx=10)
 tab_alarm = Frame(tabs)
 tab_chrono = Frame(tabs)
-tab_temp = Frame(tabs)
+tab_timer = Frame(tabs)
 tab_world = Frame(tabs)
 tab_alarm.pack()
 tab_chrono.pack()
-tab_temp.pack()
+tab_timer.pack()
 tab_world.pack()
 tabs.add(tab_alarm, text="ALARMA")
 tabs.add(tab_chrono, text="CRONÓMETRO")
-tabs.add(tab_temp, text="TEMPORIZADOR")
+tabs.add(tab_timer, text="TEMPORIZADOR")
 tabs.add(tab_world, text="RELOJ MUNDIAL")
+
 frame_alarm = Frame(tab_alarm)
 frame_alarm.pack(pady=20)
+
 frame_chrono = Frame(tab_chrono)
 frame_chrono.pack(pady=20)
 frame_laps = Frame(tab_chrono)
 frame_laps.pack()
 frame_chrono_buttons = Frame(tab_chrono)
 frame_chrono_buttons.pack(pady=20)
+
+frame_timer = Frame(tab_timer)
+frame_timer.pack(pady=20)
+frame_timer_config = Frame(tab_timer)
+frame_timer_config.pack()
+frame_timer_button = Frame(tab_timer)
+frame_timer_button.pack()
 
 # ---------------------- RELOJ -----------------------
 label_hm = Label(frame_hour, font=("Goudy Old Style", 100), text="H:M")
@@ -216,7 +286,7 @@ click_restore = False
 laps = 0
 
 label_chrono = Label(frame_chrono, font=("Goudy Old Style", 50), text="00:00:00")
-label_chrono.grid(row=0, column=0)
+label_chrono.pack()
 
 lap_1 = Label(frame_laps, font=("Goudy Old Style", 14), text="Vuelta 1")
 lap_1.grid(row=0, column=0, padx=15)
@@ -245,5 +315,40 @@ lap_button.grid(row=0, column=1)
 restore_button = Button(frame_chrono_buttons, width=20, text="Restablecer", command=restore_chrono)
 restore_button.grid(row=0, column=2)
 
+# ---------------------- TEMPORIZADOR ----------------------
+play = PhotoImage(file="img/play.png")
+pause = PhotoImage(file="img/pause.png")
+restore = PhotoImage(file="img/restore.png")
+is_running = 0
+timer_hh = "00"
+timer_mm = "00"
+timer_ss = "00"
+
+label_timer = Label(frame_timer, font=("Goudy Old Style", 50), text="00:00:00")
+label_timer.pack()
+
+timer_h = Spinbox(frame_timer_config, values=list_m, font=("Goudy Old Style", 30), width=2, state="readonly")
+timer_h.grid(row=0, column=0)
+timer_h.set(list_m[0])
+
+timer_colon_1 = Label(frame_timer_config, font=("Goudy Old Style", 30), text=":")
+timer_colon_1.grid(row=0, column=1)
+
+timer_m = Spinbox(frame_timer_config, values=list_m, font=("Goudy Old Style", 30), width=2, state="readonly")
+timer_m.grid(row=0, column=2)
+timer_m.set(list_m[0])
+
+timer_colon_2 = Label(frame_timer_config, font=("Goudy Old Style", 30), text=":")
+timer_colon_2.grid(row=0, column=3)
+
+timer_s = Spinbox(frame_timer_config, values=list_m, font=("Goudy Old Style", 30), width=2, state="readonly")
+timer_s.grid(row=0, column=4)
+timer_s.set(list_m[0])
+
+timer_play = Button(frame_timer_button, text="play", image=play, command=click_play)
+timer_play.grid(row=0, column=0)
+
+timer_restore = Button(frame_timer_button, image=restore, command=restore_timer)
+timer_restore.grid(row=0, column=1)
 
 root.mainloop()
